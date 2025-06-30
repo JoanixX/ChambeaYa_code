e3f218bf41f797a2532680f00c0cb2fb98475e63
Cesar Joaquin Alvarado Osorio <alvaradocjosorio@gmail.com>
2025-06-30 18:06:14 -0500

    Update connection.py update pass

--- app/infraestructure/database/connection.py
+++ app/infraestructure/database/connection.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 #conexion a la base de datos PostgreSQL
 ## DATABASE_URL = "postgresql+asyncpg://postgres:etc100charmander@localhost:5432/chambeaya-db"
-DATABASE_URL = "postgresql+asyncpg://admindbchambeaya:1qa.!QA.@db-postgresql-chambeaya.postgres.database.azure.com:5432/chambeaya-db?ssl=require"
+DATABASE_URL = "postgresql+asyncpg://admindbchambeaya:1qa.2ws.3ed.@db-postgresql-chambeaya.postgres.database.azure.com:5432/chambeaya-bd?ssl=require"
 ##CDATABASE_URL = "postgresql+asyncpg://admindbchambeaya%40db-postgresql-chambeaya:1qa.!QA.@db-postgresql-chambeaya.postgres.database.azure.com:5432/chambeaya-db?ssl=require"
 
 
